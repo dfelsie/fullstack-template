d56c511fa95ecc0ad1d8a96d79c08e739172d43c
dfelsie <dfelsenthal88@gmail.com>
2022-06-24 20:57:55 -0400

    Fake Step two done

--- full-ts-template-backend/src/fake.ts
+++ full-ts-template-backend/src/fake.ts
@@ -1,28 +1,75 @@
 import { faker } from "@faker-js/faker";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, User, Post } from "@prisma/client";
 // import { faker } from '@faker-js/faker/locale/de';
 const prisma = new PrismaClient();
 
-function createRandomUser(): any {
+function createRandomUser(): User {
   return {
-    userId: faker.datatype.uuid(),
-    username: faker.internet.userName(),
+    id: faker.datatype.number({ max: 10000000 }),
     email: faker.internet.email(),
-    avatar: faker.image.avatar(),
+    name: faker.internet.userName(),
+    displayName: faker.name.firstName(),
     password: faker.internet.password(),
-    birthdate: faker.date.birthdate(),
-    registeredAt: faker.date.past(),
   };
 }
 
-function users(num: number) {
-  const users: any[] = [];
-  Array.from({ length: 10 }).forEach(() => {
+async function createRandomPost(userAry: User[]): Promise<Post> {
+  return {
+    content: faker.lorem.paragraphs(),
+    createdAt: faker.datatype.datetime(),
+    id: faker.datatype.number({ max: 10000000 }),
+    published: false,
+    title: faker.address.city(),
+    updatedAt: new Date(),
+    authorName: faker.helpers.arrayElement(userAry).name,
+  };
+}
+
+async function users(num: number) {
+  const users: User[] = [];
+  Array.from({ length: num }).forEach(() => {
     users.push(createRandomUser());
   });
-  prisma.user.createMany({
+  let user: User;
+  let numFollowers;
+  users.forEach((user, index, userAry) => {
+    numFollowers = Math.ceil(Math.random() * 5);
+    for (let i = 0; i < numFollowers; i++) {
+      prisma.follows.create({
+        data: {
+          follower: {
+            connect: {
+              name: faker.helpers.arrayElement(userAry).name,
+            },
+          },
+          following: {
+            connect: {
+              name: user.name,
+            },
+          },
+        },
+      });
+    }
+  });
+  await prisma.user.createMany({
     data: users,
   });
+}
+
+async function posts(num: number) {
+  const posts: Post[] = [];
+  const dbUsers = await prisma.user.findMany();
+
+  Array.from({ length: num }).forEach(async () => {
+    posts.push(await createRandomPost(dbUsers));
+  });
+  prisma.post.createMany({
+    data: posts,
+  });
+}
+
+function clear() {
+  prisma.user.deleteMany();
 }
 
 //Add in a reset db function
